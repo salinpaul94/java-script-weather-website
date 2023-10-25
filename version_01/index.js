bd71678181317776f3817db4451f74cfe8c55a8e
salinpaul valooran <salinpv@hotmail.com>
2023-10-26 01:00:03 +0530

    api for getting weather data

---
+++ version_01/index.js
@@ -0,0 +1,6 @@
+// api key 1dd9052073524d81809a21435dc506f3
+// 2f6214368a9e52e6f08f4e60d2af1e44
+
+// Built-in API request by city name
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?q=kochi&appid=2f6214368a9e52e6f08f4e60d2af1e44&units=metric
