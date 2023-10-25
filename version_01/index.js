c2fe78209f8d88b65491e9f4e9da0332c2c7ba5b
salinpaul valooran <salinpv@hotmail.com>
2023-10-26 02:16:16 +0530

    changing weather icon and weather condition

--- version_01/index.js
+++ version_01/index.js
@@ -10,6 +10,7 @@ const api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
 const search_box = document.querySelector(".search input");
 const search_button = document.querySelector(".search button");
+const weather_icon = document.querySelector(".weather_icon");
 
 search_button.addEventListener("click", () => {
   check_weather(search_box.value);
@@ -29,10 +30,27 @@ async function check_weather(city_name) {
 
   console.log(data);
 
-  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".weather_condition").innerHTML = data.weather[0].main;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    weather_icon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weather_icon.src = "images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weather_icon.src = "images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weather_icon.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weather_icon.src = "images/mist.png";
+  } else if (data.weather[0].main == "Snow") {
+    weather_icon.src = "images/snow.png";
+  } else if (data.weather[0].main == "Haze") {
+    weather_icon.src = "images/haze.png";
+  }
 }
 
 window.addEventListener("DOMContentLoaded", check_weather("new delhi"));
